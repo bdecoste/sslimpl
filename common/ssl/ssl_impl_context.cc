a81e7b180f4b5c6db925f8a003a56f348ad2472c
root <root@localhost.localdomain>
2018-11-17 01:41:48 +0100

    update

--- common/ssl/ssl_impl_context.cc
+++ common/ssl/ssl_impl_context.cc
@@ -58,5 +58,23 @@ int set_strict_cipher_list(SSL_CTX *ctx, const char *str) {
   return 1;
 }
 
+std::string getSerialNumberFromCertificate(X509* cert) {
+  ASN1_INTEGER* serial_number = X509_get_serialNumber(cert);
+  BIGNUM* num_bn(BN_new());
+  ASN1_INTEGER_to_BN(serial_number, num_bn);
+  char* char_serial_number = BN_bn2hex(num_bn);
+  BN_free(num_bn);
+  if (char_serial_number != nullptr) {
+    std::string serial_number(char_serial_number);
+
+    // openssl is uppercase, boringssl is lowercase. So convert
+    std::transform(serial_number.begin(), serial_number.end(), serial_number.begin(), ::tolower);
+    
+    OPENSSL_free(char_serial_number);
+    return serial_number;
+  }
+  return "";
+}
+    
 } // namespace Ssl
 } // namespace Envoy
